db0dd882d25f8d62f90f8c17645f284e420bf3b7
Antonova Maria <116485404+AntonovaMS@users.noreply.github.com>
2023-06-01 18:28:25 +0300

    Update ИНДИВИД ПРОЕКТ REPORT.sql

--- ИНДИВИД ПРОЕКТ REPORT.sql
+++ ИНДИВИД ПРОЕКТ REPORT.sql
@@ -27,9 +27,9 @@ FROM (SELECT *
             WHERE t.trans_date>= A.VALID_TO +1)
 
 UNION ALL
---3) ÑÎÂÅÐØÅÍÈÅ ÎÏÅÐÀÖÈÉ Â ÐÀÇÍÛÕ ÃÎÐÎÄÀÕ Â ÒÅ×ÅÍÈÅ ÎÄÍÎÃÎ ×ÀÑÀ
+--3) СОВЕРШЕНИЕ ОПЕРАЦИЙ В РАЗНЫХ ГОРОДАХ В ТЕЧЕНИЕ ОДНОГО ЧАСА
 
-select t1. td AS FRAUD_DT, c.PASSPORT_NUM AS PASSPORT,  C.LAST_NAME ||' '|| C.FIRST_NAME ||' '|| C.PATRINYMIC AS FIO, C.PHONE AS PHONE, 'ÎÏÅÐÀÖÈB Â ÐÀÇÍÛÕ ÃÎÐÎÄÀÕ Â ÒÅ×ÅÍÈÅ ÎÄÍÎÃÎ ×ÀÑÀ ' AS FRAUD_TYPE, SYSDATE AS REPORT_DT
+select t1. td AS FRAUD_DT, c.PASSPORT_NUM AS PASSPORT,  C.LAST_NAME ||' '|| C.FIRST_NAME ||' '|| C.PATRINYMIC AS FIO, C.PHONE AS PHONE, 'ОПЕРАЦИИ В РАЗНЫХ ГОРОДАХ В ТЕЧЕНИЕ ОДНОГО ЧАСА' AS FRAUD_TYPE, SYSDATE AS REPORT_DT
 FROM (select a.trans_date as td, a.card_num as cn, a.terminal_city as tc ,b.trans_date as trans_date, b.card_num as card_num,b.terminal_city as terminal_city1, abs( a.trans_date -b.trans_date) as r , to_date('15-10-2021 06:17:37' , 'DD.MM.YYYY HH24:MI:ss') - to_date('15-10-2021 05:17:37' , 'DD.MM.YYYY HH24:MI:ss') as w  
               from (select a.* ,te.terminal_city  from fact_transactions a  join dim_terminals te  on a.terminal= te.terminal_id) a
                join (select a.* ,te.terminal_city  from fact_transactions a   join dim_terminals te  on a.terminal= te.terminal_id)  b 
@@ -44,22 +44,24 @@ JOIN DIM_CLIENTS C
 ON A.CLIENT = C.CLIENT_ID
 
 UNION ALL
-/* 4)Ïîïûòêà ïîäáîðà ñóìì.Â òå÷åíèå 20 ìèíóò ïðîõîäèò áîëåå 3õ îïåðàöèé ñî ñëåäóþùèì øàáëîíîì - êàæäàÿ
-ïîñëåäóþùàÿ ìåíüøå ïðåäûäóùåé ïðè ýòîì îòêëîíåíû âñå êðîìå ïîñëåäíåé.Ïîñëåäíÿÿ îïåðàöèÿ (óñïåøíàÿ) â òàêîé öåïî÷êå ñ÷èòàåòñÿ ìîøåííè÷åñêîé.*/
+/* 4)Попытка подбора сумм.В течение 20 минут проходит более 3х операций со следующим шаблоном - каждая
+последующая меньше предыдущей при этом отклонены все кроме последней.Последняя операция (успешная) в такой цепочке считается мошеннической.*/
 
-SELECT  t6. TRANS_DATE AS FRAUD_DT, c.PASSPORT_NUM AS PASSPORT,  C.LAST_NAME ||' '|| C.FIRST_NAME ||' '|| C.PATRINYMIC AS FIO, C.PHONE AS PHONE, 'ÏÎÏÛÒÊÀ ÏÎÄÁÎÐÀ ÑÓÌÌ ' AS FRAUD_TYPE, SYSDATE  AS REPORT_DT           
+
+SELECT  t6. TRANS_DATE AS FRAUD_DT, c.PASSPORT_NUM AS PASSPORT,  C.LAST_NAME ||' '|| C.FIRST_NAME ||' '|| C.PATRINYMIC AS FIO, C.PHONE AS PHONE, 'ПОПЫТКА ПОДБОРА СУММ' AS FRAUD_TYPE, SYSDATE  AS REPORT_DT           
 FROM(SELECT T5.*
 FROM(SELECT T4.* , SUM(CC) OVER (PARTITION BY RAN, CARD_NUM )   AS SU2       
-FROM(SELECT T3.*,  count(AMT)  OVER (PARTITION BY  CARD_NUM,  RAN ORDER BY TRANS_DATE ROWS  BETWEEN UNBOUNDED PRECEDING AND 1 PRECEDING  ) AS CO, -- ÁÎËÅÅ ÒÐÅÕ ÎÏÅÐÀÖÈÉ 
+FROM(SELECT T3.*,  count(AMT)  OVER (PARTITION BY  CARD_NUM,  RAN ORDER BY TRANS_DATE ROWS  BETWEEN UNBOUNDED PRECEDING AND 1 PRECEDING  ) AS CO, -- БОЛЕЕ ТРЕХ ОПЕРАЦИЙ  
                                   LEAD(amt) OVER (PARTITION BY card_num ORDER BY trans_date) AS LS,  
-                                  CASE WHEN  Amt>COALESCE(LEAD(amt) OVER (PARTITION BY card_num ORDER BY trans_date),0) THEN 0 ELSE 1 END AS CC -- ÂÛßÂËßÅÌ ÎÏÅÐÀÖÈÈ ÍÅ ÑÎÎÒÂÅÒÑÒÂÓÞÙÈÅ ÓÑËÎÂÈÞ ÊÀÆÄÀß ÌÅÍÜØÅ ÏÐÅÄÈÄÓÙÅÉ
-            FROM(SELECT T2.*, DENSE_RANK() OVER (PARTITION BY CARD_NUM  ORDER BY TIM) AS RAN  --ÐÀÍÆÈÐÓÅÌ ÎÏÅÐÀÖÈÈ ,ÊÎÒÎÐÛÅ ÂÕÎÄßÒ Â ÈÍÒÅÐÂÀË È ÍÅ ÂÕÎÄßÒ
+                                  CASE WHEN  Amt>COALESCE(LEAD(amt) OVER (PARTITION BY card_num ORDER BY trans_date),0) THEN 0 ELSE 1 END AS CC -- ВЫЯВЛЯЕМ ОПЕРАЦИИ НЕ СООТВЕТСТВУЮЩИЕ УСЛОВИЮ КАЖДАЯ МЕНЬШЕ ПРЕДИДУЩЕЙ
+            FROM(SELECT T2.*, DENSE_RANK() OVER (PARTITION BY CARD_NUM  ORDER BY TIM) AS RAN  --РАНЖИРУЕМ ОПЕРАЦИИ ,КОТОРЫЕ ВХОДЯТ В ИНТЕРВАЛ И НЕ ВХОДЯТ
+
                           FROM (select t.*,count(AMT)  OVER (PARTITION BY  CARD_NUM  ORDER BY TRANS_DATE ROWS  BETWEEN UNBOUNDED PRECEDING AND 1 PRECEDING  ) AS SU,
                                                    FIRST_VALUE(TRANS_DATE) OVER (PARTITION BY  CARD_NUM ORDER BY trans_date ) AS F,
                                                    FIRST_VALUE(TRANS_DATE) OVER (PARTITION BY  CARD_NUM ORDER BY trans_date ) + ( to_date('15-10-2021 06:20:00' , 'DD.MM.YYYY HH24:MI:ss') - to_date('15-10-2021 06:00:00' , 'DD.MM.YYYY HH24:MI:ss')) AS FV,
                                                    CASE WHEN TRANS_DATE BETWEEN  FIRST_VALUE(TRANS_DATE) OVER (PARTITION BY  CARD_NUM ORDER BY trans_date ) AND
                                                    FIRST_VALUE(TRANS_DATE) OVER (PARTITION BY  CARD_NUM ORDER BY trans_date ) + ( to_date('15-10-2021 06:20:00' , 'DD.MM.YYYY HH24:MI:ss') - to_date('15-10-2021 06:00:00' , 'DD.MM.YYYY HH24:MI:ss')) 
-                                                   THEN 1 ELSE 0 END AS TIM --ÂÕÎÄÈÒ ËÈ ÎÏÅÐÀÖÈß Â ÈÍÒÅÐÂÀË 20 ÌÈÍ
+                                                   THEN 1 ELSE 0 END AS TIM --ВХОДИТ ЛИ ОПЕРАЦИЯ В ИНТЕРВАЛ 20 МИН
                                        from FACT_TRANSACTIONS   t  )  t2 )T3 )T4 )T5
 WHERE SU2=0 AND t5.CO >=3 AND t5.OPER_RESULT = 'Óñïåøíî' and oper_type != 'Ïîïîëíåíèå') T6
 JOIN DIM_CARDS C
